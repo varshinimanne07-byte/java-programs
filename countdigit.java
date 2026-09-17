edeb5bd1af6060007a5df971c572c47d5f747583
varshinimanne07-byte <varshinimanne07@gmail.com>
2026-09-17 17:05:06 +0530

    Implement digit counting method in Solution class

---
+++ countdigit.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countDigits(int num) {
+        int n = num;
+        int count = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+
+            if (num % digit == 0)
+                count++;
+
+            n = n / 10;
+        }
+
+        return count;
+    }
+}
